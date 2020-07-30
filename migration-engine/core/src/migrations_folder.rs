92794ad9066a270e71e77ac584abb92f8408fb12
Tom Houlé <tom@tomhoule.com>
2020-07-30 17:31:40 +0200

    Iterate on migrations folder API

--- migration-engine/core/src/migrations_folder.rs
+++ migration-engine/core/src/migrations_folder.rs
@@ -5,6 +5,7 @@
 //! - A migration script
 
 use std::{
+    ffi::OsStr,
     fs::{create_dir, read_dir, DirEntry},
     io,
     path::{Path, PathBuf},
@@ -14,7 +15,10 @@ use std::{
 pub const MIGRATION_SCRIPT_FILENAME: &str = "migration";
 
 /// Create a folder for a new migration.
-pub(crate) fn create_migration_folder(migrations_folder_path: &Path, migration_name: &str) -> io::Result<PathBuf> {
+pub(crate) fn create_migration_folder(
+    migrations_folder_path: &Path,
+    migration_name: &str,
+) -> io::Result<MigrationFolder> {
     let timestamp = chrono::Utc::now().format("%Y%m%d%M%S");
     let folder_name = format!(
         "{timestamp}_{migration_name}",
@@ -35,21 +39,39 @@ pub(crate) fn create_migration_folder(migrations_folder_path: &Path, migration_n
 
     create_dir(&folder_path)?;
 
-    Ok(folder_path)
+    Ok(MigrationFolder(folder_path))
 }
 
-/// List the migrations present in the migration folder.
-pub(crate) fn list_migrations(
-    migrations_folder_path: &Path,
-) -> io::Result<impl Iterator<Item = io::Result<MigrationFolder>>> {
-    let entries = read_dir(migrations_folder_path)?.filter_map(|entry_result| match entry_result {
-        Ok(entry) if entry.file_type().ok()?.is_dir() => Some(Ok(MigrationFolder(entry))),
-        Ok(_entry) => None,
-        Err(err) => Some(Err(err)),
-    });
+/// List the migrations present in the migration folder, ordered by increasing timestamp.
+pub(crate) fn list_migrations(migrations_folder_path: &Path) -> io::Result<Vec<MigrationFolder>> {
+    let mut entries: Vec<MigrationFolder> = Vec::new();
+
+    for entry in read_dir(migrations_folder_path)? {
+        let entry = entry?;
+
+        if entry.file_type()?.is_dir() {
+            entries.push(entry.into());
+        }
+    }
+
+    entries.sort_by(|a, b| a.migration_id().cmp(b.migration_id()));
 
     Ok(entries)
 }
 
-/// Proxy to a folder containing one migration, as returned by `list_migrations`.
-pub(crate) struct MigrationFolder(DirEntry);
+/// Proxy to a folder containing one migration, as returned by
+/// `create_migration_folder` and `list_migrations`.
+pub(crate) struct MigrationFolder(PathBuf);
+
+impl MigrationFolder {
+    /// The `{timestamp}_{name}` formatted migration id.
+    pub(crate) fn migration_id(&self) -> &OsStr {
+        self.0.file_name().expect("MigrationFolder::migration_id")
+    }
+}
+
+impl From<DirEntry> for MigrationFolder {
+    fn from(entry: DirEntry) -> MigrationFolder {
+        MigrationFolder(entry.path())
+    }
+}
